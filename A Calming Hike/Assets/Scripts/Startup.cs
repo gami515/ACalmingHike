5ec3d90346f199d39d3b84e704e6a8013650d9f5
Gabriel-Miller-Student <gami515@student.oaisd.org>
2022-10-28 08:27:53 -0400

    Menu

---
+++ A Calming Hike/Assets/Scripts/Startup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+
+[InitializeOnLoad]
+public class Startup
+{
+    static Startup()
+    {
+        Debug.Log("Up and running");
+    }
+}
